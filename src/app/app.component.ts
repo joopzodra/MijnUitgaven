2455380adaba90644fb8f073afeabedcea7c464f
Joop Rosier <joop@zodra.nl>
2016-12-21 18:44:14 +0100

    Splashscreen.hide() in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,8 @@ export class MyApp {
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-      Splashscreen.show();
+      Splashscreen.hide();
     });
   }
 }
